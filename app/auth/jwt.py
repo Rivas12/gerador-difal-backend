b0fbf0b0f1f4f442b2ce1bd5ea9f1889fb62730d
Rivaldo Silveira <rivaldosilveira13@gmail.com>
2025-06-21 09:41:33 -0300

    Refactor JWT token expiration handling to use timezone-aware datetime

--- app/auth/jwt.py
+++ app/auth/jwt.py
@@ -1,17 +1,18 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import jwt
 import os
 
-SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_secret_key')
+SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 ALGORITHM = os.getenv('JWT_ALGORITHM', 'HS256')
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 30))
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 200))
 
 def createAccessToken(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
+    now_utc = datetime.now(timezone.utc)
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = now_utc + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+        expire = now_utc + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
