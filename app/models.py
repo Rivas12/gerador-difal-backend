354d0b9583c602ec7b9ac7cca60fda4c16d5ed50
Rivaldo Silveira <rivaldosilveira13@gmail.com>
2025-06-21 21:19:45 -0300

    Refactor: aprimorar o modelo Usuario com tratamento de senha e registro de data e hora com reconhecimento de fuso horário

--- app/models.py
+++ app/models.py
@@ -1,12 +1,32 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+def now_sp():
+    tz = pytz.timezone('America/Sao_Paulo')
+    return datetime.now(tz)
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(100), nullable=False)  # Adicionado nullable=False
+    username = db.Column(db.String(100), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    _senha = db.Column('senha', db.String(255), nullable=False)
+    data_created = db.Column(db.DateTime, nullable=False, default=now_sp)
+
+    @property
+    def senha(self):
+        return self._senha
+
+    @senha.setter
+    def senha(self, senha_plana):
+        self._senha = generate_password_hash(senha_plana)
+
+    def verificar_senha(self, senha_plana):
+        return check_password_hash(self._senha, senha_plana)
 
     def __repr__(self):
-        return f'<Usuario {self.nome}>'
+        return f'<Usuario {self.username}>'
--- run.py
+++ run.py
@@ -14,5 +14,8 @@ def create_app():
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()  # Cria as tabelas se não existirem
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
